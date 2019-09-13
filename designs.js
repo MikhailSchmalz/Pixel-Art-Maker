0d9e3ecb6f8fdebc5dd7974a5d5ef3c1771494aa
Mikhail Schmalzried <mikhail@mikhailschmalzried.com>
2019-09-13 14:42:06 -0700

    Add Files Via Upload
    Added all files via upload. HTML, styles and readme are from the supplied files by Udacity.

---
+++ designs.js
@@ -0,0 +1,34 @@
+// Select color input and store in variable selColor
+let selColor = document.getElementById("colorPicker");
+
+// Select size input and store in variable selSize
+var selSize = document.getElementById("sizePicker");
+
+// Define variables and store values for DOM elements
+var pixelCanvas = document.getElementById("pixelCanvas");
+
+selSize.addEventListener("submit", function(event) {
+    event.preventDefault();
+    makeGrid();
+});
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+var height = document.getElementById("inputHeight").value;
+var width = document.getElementById("inputWidth").value;
+pixelCanvas.innerHTML = null;
+
+//loop over each row and column or cell and change its color based on selected color
+for (var h = 0; h < height; h++) {
+    var row = pixelCanvas.insertRow(h);
+    for (var w = 0; w < width; w++) {
+        var cell = row.insertCell(h);
+        cell.addEventListener('click', function(event) {
+            // change color of cell when it is clicked
+            event.target.style.backgroundColor = selColor.value;
+        });
+      }
+    }
+}
